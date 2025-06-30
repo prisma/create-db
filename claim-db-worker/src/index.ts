23a66fa3a2748e4e0a3d307bd4684421a336568f
Aidan <aidankmcalister@gmail.com>
2025-06-30 09:21:20 -0500

    readme added

--- claim-db-worker/src/index.ts
+++ claim-db-worker/src/index.ts
@@ -1,18 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const connectionString = url.searchParams.get('connectionString');
+		if (!connectionString) {
+			return new Response('No connection string provided.', { status: 400 });
+		}
+		return new Response(`Connection String successfully sent to worker: ${connectionString}`);
 	},
 } satisfies ExportedHandler<Env>;
