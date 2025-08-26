c4918ecb3396c5c3c20bcc1263b27eccb42f3d8c
Aidan McAlister <aidankmcalister@gmail.com>
2025-08-26 12:59:54 -0400

    fix: push and pull buttons error fix

--- claim-db-worker/components/PrismaSchemaEditor.tsx
+++ claim-db-worker/components/PrismaSchemaEditor.tsx
@@ -1241,19 +1241,16 @@ const PrismaSchemaEditor = ({
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
                 >
                   <path d="M19 16v6m0 0l3-3m-3 3l-3-3M4 6v6s0 3 7 3s7-3 7-3V6" />
                   <path d="M11 3c7 0 7 3 7 3s0 3-7 3s-7-3-7-3s0-3 7-3m0 18c-7 0-7-3-7-3v-6" />
                 </g>
               </svg>
             )}
-            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1 lg:hidden">
-              Pull
-            </span>
-            <span className="text-xs font-bold mt-1 hidden lg:block">Pull</span>
+            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1">Pull</span>
           </button>
 
           <button
@@ -1289,19 +1286,16 @@ const PrismaSchemaEditor = ({
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
                 >
                   <path d="M4 6v6s0 3 7 3s7-3 7-3V6" />
                   <path d="M11 3c7 0 7 3 7 3s0 3-7 3s-7-3-7-3s0-3 7-3m0 18c-7 0-7-3-7-3v-6m15 10v-6m0 0l3 3m-3-3l-3 3" />
                 </g>
               </svg>
             )}
-            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1 lg:hidden">
-              Push
-            </span>
-            <span className="text-xs font-bold mt-1 hidden lg:block">Push</span>
+            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1">Push</span>
           </button>
 
           <button
@@ -1339,17 +1333,14 @@ const PrismaSchemaEditor = ({
                 <path
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
                   d="m14.363 5.652l1.48-1.48a2 2 0 0 1 2.829 0l1.414 1.414a2 2 0 0 1 0 2.828l-1.48 1.48m-4.243-4.242l-9.616 9.615a2 2 0 0 0-.578 1.238l-.242 2.74a1 1 0 0 0 1.084 1.085l2.74-.242a2 2 0 0 0 1.24-.578l9.615-9.616m-4.243-4.242l4.243 4.242"
                 />
               </svg>
             )}
-            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1 lg:hidden">
-              Format
-            </span>
-            <span className="text-xs font-bold mt-1 hidden lg:block">
+            <span className="text-xs font-bold ml-2 lg:ml-0 lg:mt-1">
               Format
             </span>
           </button>
