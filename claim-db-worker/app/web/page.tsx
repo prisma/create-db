3e47543a4bf6f3df9da01399145a2818ac40633b
Aidan McAlister <aidankmcalister@gmail.com>
2025-08-29 07:56:48 -0400

    fix: landing on `/web` creates a db and redirects you to `/web/connect`

--- claim-db-worker/app/web/page.tsx
+++ claim-db-worker/app/web/page.tsx
@@ -1,20 +1,18 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { useDatabase } from "./DatabaseContext";
 
 export default function WebPage() {
   const router = useRouter();
   const { dbInfo } = useDatabase();
-  
+
   useEffect(() => {
-    // Only redirect to connect page if we have a connection string
     if (dbInfo?.connectionString) {
       router.push("/web/connect");
     }
   }, [dbInfo, router]);
-  
-  // Show nothing while checking
+
   return null;
 }
